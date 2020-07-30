74df9f84ae02eed86a4c334f4b0786da340b3375
Yoshitaka Matsuda <yoshitaka.matsuda@blastedge-games.co.jp>
2020-07-30 19:00:24 +0900

    Switch function name log either blueprint or native

--- DILoggerPlugin/Source/DILoggerPlugin/Private/DILogHandler.cpp
+++ DILoggerPlugin/Source/DILoggerPlugin/Private/DILogHandler.cpp
@@ -3,23 +3,26 @@
 
 void UDILogHandler::Log(const FString& Message, const void* Context, const FString& FileName, int32 Line, const FString& CalledFunction, const FLogCategoryBase* Category, ELogVerbosity::Type Verbosity, bool WithAssertion, bool ToScreen, float TimeToDisplay, const FColor& DisplayColor, const FVector2D& DisplayTextScale)
 {
-	FString DetailMessage;
-	if (Context != nullptr)
+	FString _CalledFunction;
+	if (Line == 0 /* From Blueprint */)
 	{
-		UObject* ObjectContext = reinterpret_cast<UObject*>(const_cast<void*>(Context));
-		if (IsValid(ObjectContext))
+		TArray<FString> Callstacks;
+		FFrame::GetScriptCallstack().ParseIntoArray(Callstacks, L"\n");
+		if (Callstacks.Num() > 1)
 		{
-			DetailMessage += ObjectContext->GetName() + ": ";
+			_CalledFunction = Callstacks.Last();
 		}
 	}
-	DetailMessage += Message + TEXT("\n") + FFrame::GetScriptCallstack();
-
-	FMsg::Logf(TCHAR_TO_ANSI(*FileName), Line, Category->GetCategoryName(), Verbosity, TEXT("%s\n\t-FILE: %s:%d\n\t-FUNCTION: %s"), *DetailMessage, *FileName, Line, *CalledFunction);
+	else
+	{
+		_CalledFunction = CalledFunction;
+	}
+	FMsg::Logf(TCHAR_TO_ANSI(*FileName), Line, Category->GetCategoryName(), Verbosity, TEXT("%s\n\t-FILE: %s:%d\n\t-FUNCTION: %s"), *Message, *FileName, Line, *_CalledFunction);
 
 	if (WithAssertion)
 	{
 #if WITH_EDITOR
-		FMessageDialog::Open(EAppMsgType::Ok, FText::FromString(DetailMessage));
+		FMessageDialog::Open(EAppMsgType::Ok, FText::FromString(Message));
 #endif
 	}
 
